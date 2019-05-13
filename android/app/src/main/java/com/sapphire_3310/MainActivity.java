e566f423471c2ff9e12c211a1d69579a02cb8f3b
Crowdbotics <team@crowdbotics.com>
2019-05-13 22:45:47 +0000

    initial commit

---
+++ android/app/src/main/java/com/sapphire_3310/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sapphire_3310;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sapphire_3310";
+    }
+}
